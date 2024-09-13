c8a3db09e97b453dc79429acbd9bcc11fc1cc5c7
Ramon Rasheed <ramonrash2@gmail.com>
2024-09-13 08:11:00 +0100

    Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#eab308",
+        secondary: "#0d9488",
+      },
       fontFamily: {
         sans: ["Montserrat", "sans-serif"],
       },
