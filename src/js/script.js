84ec27caef68cafdfea71303e499affbc015fe48
Ramon Rasheed <ramonrash2@gmail.com>
2024-09-14 15:08:57 +0100

    Patners-page

--- src/js/script.js
+++ src/js/script.js
@@ -21,4 +21,21 @@ $(document).ready(function () {
     // Toggle the clicked accordion content
     $(this).next(".accordion-content").slideToggle();
   });
+
+  $(".tab-links a").click(function (e) {
+    e.preventDefault();
+
+    // Remove active class from all tabs and hide their content
+    $(".tab-links a").removeClass("bg-secondary text-white");
+    $(".tab-links a").addClass("bg-gray-300 text-balck");
+    $(".tab-content .tab").addClass("hidden");
+    $(".tab-content .tab").removeClass("flex");
+
+    // Add active class to the clicked tab and show its content
+    $(this).removeClass("bg-gray-300 text-black");
+    $(this).addClass("bg-secondary text-white");
+    var currentTab = $(this).attr("href");
+    $(currentTab).removeClass("hidden");
+    $(currentTab).addClass("flex");
+  });
 });
